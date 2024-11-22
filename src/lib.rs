a0f6ad182c52bcfca5db30f3db8525131e9e5676
Julius Max <rust@juliusmax.net>
2024-11-22 16:57:41 +0100

    Sart postgrest connections

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
+const FUNCTION_LOGIN = "login";
+const LOGIN_FIELD = "login";
+const PWD_FIELD = "pass";
 
+use postgrest::Postgrest;
+use serde_json::{ Map, Value};
+
+#[tokio::main]
+async fn get_token(uri:&str, login:&str, pwd:&str) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Postgrest::new(uri);
+    let credentials = vec![vec![LOGIN_FIELD, login], vec![PWD_FIELD, &pwd]];
+    credentials.iter().map(|c|{})
+    let resp = client.rpc(FUNCTION_LOGIN, r#"{"login":{}}"#).await?;
+    println!("{}", resp.text().await?);
+    Ok(())
+}
